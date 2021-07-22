5cd9d07af31d9671015073e4d0ec6df0e37b4485
Carlos Vendrell <carlos.vendrell@tigerspike.com>
2021-07-22 21:33:20 +0200

    setting up DI with Hilt Android (Created new Runner and configured dependencies)

--- app/src/androidTest/java/com/example/recrutask/core/TestSetup.kt
+++ app/src/androidTest/java/com/example/recrutask/core/TestSetup.kt
@@ -1,14 +1,20 @@
 package com.example.recrutask.core
 
 import androidx.test.platform.app.InstrumentationRegistry
+import dagger.hilt.android.testing.HiltAndroidRule
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 
 open class TestSetup {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
 
     @Before
     fun setUp() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         Assert.assertEquals("com.example.recrutask", appContext.packageName)
+        hiltRule.inject()
     }
 }
